620b583143d9ec6d2d9c8c866929e8bf6ded9218
aces28 <aces28@gmail.com>
2013-10-12 19:35:25 +0800

    Added service component example

--- src/main/java/com/sudocode/login/service/LoginService.java
+++ src/main/java/com/sudocode/login/service/LoginService.java
@@ -2,6 +2,7 @@ package com.sudocode.login.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import com.sudocode.maximus.logger.Logger;
 
@@ -14,9 +15,21 @@ import com.sudocode.maximus.logger.Logger;
 public class LoginService extends Service {
 
     private static final Logger logger = Logger.getLogger(LoginService.class);
+    private IBinder mIBinder = new LoginBinder();
 
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return mIBinder;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    private class LoginBinder extends Binder {
+        LoginService getLocalService() {
+            return LoginService.this;
+        }
     }
 }
